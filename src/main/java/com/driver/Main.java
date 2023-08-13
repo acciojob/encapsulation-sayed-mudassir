bb0bbeceddd0099734082bb3a0aa6fe18b510de7
sayed-mudassir <sayedmuddassir68@gmail.com>
2023-08-14 00:40:28 +0530

    Attempt 1

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,14 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        RWOnly r = new RWOnly();
+//        r.name = "Mudassir";
+//        System.out.print(r.name);
+//        this will show us error
+//        error: name has private access in com.driver.RWOnly
+        r.setName("Mudassir");
+        String name = r.getName();
+
+    }
 }
